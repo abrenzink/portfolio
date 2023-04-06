95efde5045698b5872969c639824d47a650c9619
Andrea Brenzink <57133545+abrenzink@users.noreply.github.com>
2023-04-06 19:21:08 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDay()} ${monthName} ${year}`;
+
+const lastUpdate = document.lastModified;
+
+
+document.getElementById("currentYear").textContent = year;
+document.getElementById("lastUpdate").textContent = lastUpdate;
